28d70754b75c4e66a31ff873e04fc95f912001b9
Subrata Mandal <mandalsiltu35@gmail.com>
2022-05-05 00:00:05 +0530

    subrata donarlist

--- app/src/main/java/com/example/wastefoodmanagement/DonationReceiverpage.java
+++ app/src/main/java/com/example/wastefoodmanagement/DonationReceiverpage.java
@@ -2,7 +2,9 @@ package com.example.wastefoodmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DonationReceiverpage extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class DonationReceiverpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation_receiverpage);
+    }
+
+    public void openDonationReceiverpage(View view) {
+        startActivity(new Intent(this,Donar_Page.class));
     }
 }
